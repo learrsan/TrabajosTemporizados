21404742f22aead639a49e5f73bac0feac24cb44
learrsan <gaslas@hotmail.com>
2015-04-06 14:02:05 +0000

    06/04/2015 16:01

---
+++ TrabajosTemporizadosG/TrabajosJob.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.Deployment;
+
+namespace TrabajosTemporizadosG
+{
+    public class TrabajosJob : SPJobDefinition
+    {
+        public TrabajosJob()
+        {
+
+        }
+
+        public TrabajosJob(String name, SPWebApplication app, SPServer server, SPJobLockType bloqueo)
+            : base(name, app, server, bloqueo)
+        {
+            using (SPSite sitio = new SPSite("http://spcursovm"))
+            {
+                using (SPWeb web = sitio.RootWeb)
+                {
+                    SPList lista = web.Lists["TrabajosPendientes"];
+
+                    foreach (SPListItem item in lista.Items)
+                    {
+                        if (item["finalizado"] == "Yes")
+                        {
+                            
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
